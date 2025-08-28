276843aefa73bcf8daf9876f66388e6698e7b77c
Leesa Ward <leesa@doubleedesign.com.au>
2025-08-28 22:09:05 +1000

    Fix(WP ACF): Show "New page" in page header if there is no title

--- src/templates/page-header.php
+++ src/templates/page-header.php
@@ -4,7 +4,9 @@ use Doubleedesign\Comet\WordPress\Classic\TemplateHandler;
 use Doubleedesign\Comet\Core\{PageHeader};
 
 $attributes = TemplateHandler::transform_fields_to_comet_attributes($fields);
-$heading = !empty($attributes['component']['heading']) ? $attributes['component']['heading'] : get_the_title();
+$heading = !empty($attributes['component']['heading'])
+    ? $attributes['component']['heading']
+    : (!empty(get_the_title()) ? get_the_title() : 'New Page');
 if (class_exists('Doubleedesign\Breadcrumbs\Breadcrumbs') && $attributes['component']['showBreadcrumbs']) {
     $breadcrumbs = Doubleedesign\Breadcrumbs\Breadcrumbs::$instance->get_raw_breadcrumbs();
 }
