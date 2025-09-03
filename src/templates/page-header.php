c51d5f9246bdcee51ecc2b5704e0fc226f46f8ff
Leesa Ward <leesa@doubleedesign.com.au>
2025-09-03 21:53:58 +1000

    Fix(WP ACF): Check for showBreadcrumbs before using it in PageHeader

--- src/templates/page-header.php
+++ src/templates/page-header.php
@@ -7,7 +7,7 @@ $attributes = TemplateHandler::transform_fields_to_comet_attributes($fields);
 $heading = !empty($attributes['component']['heading'])
     ? $attributes['component']['heading']
     : (!empty(get_the_title()) ? get_the_title() : 'New Page');
-if (class_exists('Doubleedesign\Breadcrumbs\Breadcrumbs') && $attributes['component']['showBreadcrumbs']) {
+if (class_exists('Doubleedesign\Breadcrumbs\Breadcrumbs') && isset($attirbutes['component']['showBreadcrumbs']) && $attributes['component']['showBreadcrumbs']) {
     $breadcrumbs = Doubleedesign\Breadcrumbs\Breadcrumbs::$instance->get_raw_breadcrumbs();
 }
 
