fdb1cfae8cd825bab31620c67aa942c5a9e7f9fb
Leesa Ward <leesa@doubleedesign.com.au>
2025-09-18 13:59:28 +1000

    Fix config and docs

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,6 +1,6 @@
 <?php return array(
     'root' => array(
-        'name' => 'doubleedesign/comet-plugin',
+        'name' => 'doubleedesign/comet-plugin-acf',
         'pretty_version' => '0.1.0',
         'version' => '0.1.0.0',
         'reference' => null,
@@ -29,15 +29,17 @@
             'dev_requirement' => false,
         ),
         'doubleedesign/comet-components-core' => array(
-            'pretty_version' => '0.2.0',
-            'version' => '0.2.0.0',
-            'reference' => 'a739f7ee1ec549cdad15818f5d6fc23837de240c',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7fd4bca9715d28ac3b58bdb4b6f635f6ef0e4f84',
             'type' => 'library',
             'install_path' => __DIR__ . '/../doubleedesign/comet-components-core',
-            'aliases' => array(),
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
             'dev_requirement' => false,
         ),
-        'doubleedesign/comet-plugin' => array(
+        'doubleedesign/comet-plugin-acf' => array(
             'pretty_version' => '0.1.0',
             'version' => '0.1.0.0',
             'reference' => null,
